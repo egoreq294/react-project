2764a898d78e14acd1a7f63f2e150b17c069c428
egoreq <egoreq@bk.ru>
2020-09-17 20:09:12 +0400

    feat:  add  fetch

--- src/components/hhWidget/HhWidget.js
+++ src/components/hhWidget/HhWidget.js
@@ -2,19 +2,16 @@ import React, { useState, useEffect } from "react";
 
 function HhWidget(props) {
   const [vacancies, setVacancies] = useState({});
-  const [isResponse, setIsResponse] = useState(false);
 
   useEffect(() => {
     function getVacancies() {
       fetch("https://api.hh.ru/vacancies?employer_id=1980984")
         .then((response) => response.json())
-        .then((response) => setVacancies(response))
-        .then((response) => setIsResponse(true))
-        .catch((error) => alert("Произошла ошибка загрузки данных"));
+        .then((response) => setVacancies(response));
     }
     getVacancies();
-  }, [isResponse]);
-
+  }, []);
+  console.log(vacancies);
   return (
     <React.Fragment>
       <div></div>
